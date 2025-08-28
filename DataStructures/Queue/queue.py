d3b64441cfd743b7da9d549a3e0ed1d665e6ca52
Angie Dahana <a.apontea@uniandes.edu.co>
2025-08-28 08:52:37 -0500

    2 fun

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,1 +1,7 @@
-from DataStructures.List import list_node as lt
+from DataStructures.List import single_linked_list as lt
+def enqueue(my_queue, element):
+    lt.add_last(my_queue,element)
+    return my_queue
+
+def dequeue(my_queue):
+    return lt.delete_element(my_queue,0)
