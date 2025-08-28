7be414e98fbd31c98b05183c8c034bb675649087
Santisar0 <s.sarmientop234@github.com>
2025-08-28 09:01:15 -0500

    probA

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,1 +1,8 @@
-from DataStructures.List import list_node as lt
+from DataStructures.List import single_linked_list as lt
+
+def size(queue):
+    return lt.size(queue)
+
+def peek(queue):
+    return lt.first_element(queue)
+
